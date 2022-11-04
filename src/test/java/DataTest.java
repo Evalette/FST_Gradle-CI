d8e72e336442ace8be444f6e571a06231d4edbf2
s4elvale <s4elvale@uni-trier.de>
2022-11-04 10:44:55 +0100

    new TestCase

--- src/test/java/DataTest.java
+++ src/test/java/DataTest.java
@@ -1,6 +1,6 @@
+import com.google.gson.Gson;
 import de.unitrier.st.core.CountryDataPoint;
 import de.unitrier.st.core.DataLoader;
-import de.unitrier.st.core.DataParser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,9 +10,20 @@ import java.util.Map;
 
 public class DataTest {
 
+
     @Test
     public void tmpTest() {
         Assert.assertEquals(true, true);
     }
 
+    @Test //Aufgabe 2 "sinnvoller" Test
+    public void numDataPointsTest() {
+        DataLoader dataLoader = new DataLoader();
+        Gson gson = new Gson();
+
+        String json = dataLoader.loadJsonFile("countries-aggregated_json.json");
+        CountryDataPoint[] dataPoints = gson.fromJson(json, CountryDataPoint[].class);
+        Assert.assertEquals(dataPoints.length, 143668);
+        //Assert.assertEquals(dataPoints.length, 42);
+    }
 }
