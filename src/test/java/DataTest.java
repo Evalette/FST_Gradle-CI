50909b2a708d5f7f9aa4ac9eb158181ded984d4c
s4elvale <s4elvale@uni-trier.de>
2022-11-04 11:15:18 +0100

    fixed test

--- src/test/java/DataTest.java
+++ src/test/java/DataTest.java
@@ -23,7 +23,7 @@ public class DataTest {
 
         String json = dataLoader.loadJsonFile("countries-aggregated_json.json");
         CountryDataPoint[] dataPoints = gson.fromJson(json, CountryDataPoint[].class);
-        //Assert.assertEquals(dataPoints.length, 143668);
-        Assert.assertEquals(dataPoints.length, 42);
+        Assert.assertEquals(dataPoints.length, 143668);
+        //Assert.assertEquals(dataPoints.length, 42);
     }
 }
